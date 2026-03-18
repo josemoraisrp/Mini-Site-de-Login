bfa5f8eacd27d09ec1762fbe032eed63b11d8671
José Morais <RPMORAYS@GMAIL.COM>
2026-03-18 20:42:33 -0300

    Refactor login function to show dashboard on success

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function login() {
     let senhaDigitada = document.getElementById("senha").value;
-
     let senhaCorreta = "1234";
     let bloqueado = false;
 
-    let mensagem = document.getElementById("mensagem");
-
     if (senhaDigitada === senhaCorreta && !bloqueado) {
-        mensagem.innerText = "Acesso liberado";
-        mensagem.style.color = "green";
+        document.querySelector(".container").style.display = "none";
+        document.getElementById("dashboard").style.display = "block";
     } else {
-        mensagem.innerText = "Acesso negado";
-        mensagem.style.color = "red";
+        let msg = document.getElementById("mensagem");
+        msg.innerText = "Acesso negado ❌";
+        msg.style.color = "red";
     }
+}
+
+function logout() {
+    document.querySelector(".container").style.display = "block";
+    document.getElementById("dashboard").style.display = "none";
+
+    document.getElementById("senha").value = "";
+}
+
+function mostrar(pagina) {
+    let paginas = document.querySelectorAll(".pagina");
+
+    paginas.forEach(p => {
+        p.classList.remove("ativa");
+    });
+
+    let atual = document.getElementById(pagina);
+
+    setTimeout(() => {
+        atual.classList.add("ativa");
+    }, 50);
 }
